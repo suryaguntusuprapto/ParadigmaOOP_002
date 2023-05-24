9ae47afd348cfa0a31f25249e489cf911220ed3f
suryaguntursuprapto <suryaguntur2000@gmail.com>
2023-05-24 16:27:41 +0700

    membuat class mahasiswa

---
+++ Memberinitlist/Memberinitlist.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) {
+		//definisi
+	}
+
+	~mahasiswa() {
+		cout << "Id    = " << id << endl;
+		cout << "Nama  = " << nama << endl;;
+		cout << "Nilai = " << nilai << endl;
+	}
+};
